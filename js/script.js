cb62fc55b8a17c75ff8c09252cb2eb56df8d7bb2
Muratool <bmurat.acc.for.u.study@gmail.com>
2024-01-19 01:27:31 +0300

    19.01.2024 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 'use strict';
 
 new Swiper('.image-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+		// renderBullet: function (index, className) {
+		// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+		// },
+
+		// type: 'fraction',
+		// renderFraction: function (currentClass, totalClass) {
+		//   return 'Фото <span class="' + currentClass +'">' + '</span>' + ' из ' + '<span class = "' + totalClass +'"></span';
+		// }
+
+		// type: 'progressbar'
+	},
+
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		draggable: true,
+	},
+
+	simulateTouch: true,
 });
